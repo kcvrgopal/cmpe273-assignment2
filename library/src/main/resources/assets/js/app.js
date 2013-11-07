deddf01ab655cd2887e3032287d18532a940b6ad
kcvrgopal <mailmerajagopalan@gmail.com>
2013-11-06 21:33:35 -0800

    Updated WebSockets Implementation!

--- library/src/main/resources/assets/js/app.js
+++ library/src/main/resources/assets/js/app.js
@@ -1,13 +1,12 @@
 $(":button").click(function() {
-	var isbn = this.id;
-	alert('About to report lost on ISBN ' + isbn);
-	$.ajax({
-		url: "v1/books/"+isbn+"?status=lost",
-		type: 'PUT',
-		contentType: 'application/json',
-		success: function(result) {
-			document.getElementById(isbn+":status").innerHTML="lost";
-			document.getElementById(isbn).disabled = true;
-		}
-	});
+    var isbn = this.id;
+    alert('About to report lost on ISBN ' + isbn);
+    $.ajax({
+    url: "/library/v1/books/"+isbn+"?status=lost",
+    type: 'PUT',
+    contentType: 'application/json',
+    success: function(result) {
+        window.location.reload();
+    }
+    });
 });
