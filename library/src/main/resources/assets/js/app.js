01d80f90ab4fd0470eb1b50d23fcd6a8e4cfd53d
kcvrgopal <mailmerajagopalan@gmail.com>
2013-11-06 21:39:29 -0800

    Revert "Updated WebSockets Implementation!"
    This reverts commit deddf01ab655cd2887e3032287d18532a940b6ad.

--- library/src/main/resources/assets/js/app.js
+++ library/src/main/resources/assets/js/app.js
@@ -1,12 +1,13 @@
 $(":button").click(function() {
-    var isbn = this.id;
-    alert('About to report lost on ISBN ' + isbn);
-    $.ajax({
-    url: "/library/v1/books/"+isbn+"?status=lost",
-    type: 'PUT',
-    contentType: 'application/json',
-    success: function(result) {
-        window.location.reload();
-    }
-    });
+	var isbn = this.id;
+	alert('About to report lost on ISBN ' + isbn);
+	$.ajax({
+		url: "v1/books/"+isbn+"?status=lost",
+		type: 'PUT',
+		contentType: 'application/json',
+		success: function(result) {
+			document.getElementById(isbn+":status").innerHTML="lost";
+			document.getElementById(isbn).disabled = true;
+		}
+	});
 });
